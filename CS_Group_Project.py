2803664e82f534f579c1ee0746001cb5c1094316
Lars Pawlowski <larspaw@gmx.ch>
2026-04-21 14:08:52 +0200

    Mission Statement

--- CS_Group_Project.py
+++ CS_Group_Project.py
@@ -12,9 +12,16 @@ st.title("Welcome to AutoAppraise!")
 st.write("""
 Welcome to AutoAppraise, the ultimate tool for car enthusiasts and buyers alike! Our mission is to provide you with accurate and up-to-date information about your car's true value. Whether you're looking to sell, buy, or simply curious about your vehicle's worth, AutoAppraise has got you covered. With our user-friendly interface and powerful algorithms, you can easily get an estimate of your car's value based on its make, model, year, mileage, and condition. Join us on this exciting journey to discover the true value of your car and make informed decisions in the automotive market!
 """)
+# Our Mission
+if st.button("About Us"):
+    st.write("This is a Streamlit tutorial for FCS bachelor students!")
+    st.write("""
+Welcome to AutoAppraise, the ultimate tool for car enthusiasts and buyers alike! Our mission is to provide you with accurate and up-to-date information about your car's true value. Whether you're looking to sell, buy, or simply curious about your vehicle's worth, AutoAppraise has got you covered. With our user-friendly interface and powerful algorithms, you can easily get an estimate of your car's value based on its make, model, year, mileage, and condition. Join us on this exciting journey to discover the true value of your car and make informed decisions in the automotive market!
+""")
+
 
 # Text Input
-st.subheader("Text Input")
+st.subheader("Discover Your Car's True Value")
 name = st.text_input("Enter your name", placeholder="Your name here...")
 if name != "":
     st.write(f"Hello, {name}! Welcome to FCS!")
