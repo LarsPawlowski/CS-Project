3e06c318217f84de2d8f6ed811ab58ab6914ef08
Lars Pawlowski <larspaw@gmx.ch>
2026-04-21 14:09:39 +0200

    Updated Layout

--- CS_Group_Project.py
+++ CS_Group_Project.py
@@ -9,12 +9,9 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="AutoAppraise - Your Cars True Value", page_icon="🏎️")
 # Title & Intro
 st.title("Welcome to AutoAppraise!")
-st.write("""
-Welcome to AutoAppraise, the ultimate tool for car enthusiasts and buyers alike! Our mission is to provide you with accurate and up-to-date information about your car's true value. Whether you're looking to sell, buy, or simply curious about your vehicle's worth, AutoAppraise has got you covered. With our user-friendly interface and powerful algorithms, you can easily get an estimate of your car's value based on its make, model, year, mileage, and condition. Join us on this exciting journey to discover the true value of your car and make informed decisions in the automotive market!
-""")
+
 # Our Mission
 if st.button("About Us"):
-    st.write("This is a Streamlit tutorial for FCS bachelor students!")
     st.write("""
 Welcome to AutoAppraise, the ultimate tool for car enthusiasts and buyers alike! Our mission is to provide you with accurate and up-to-date information about your car's true value. Whether you're looking to sell, buy, or simply curious about your vehicle's worth, AutoAppraise has got you covered. With our user-friendly interface and powerful algorithms, you can easily get an estimate of your car's value based on its make, model, year, mileage, and condition. Join us on this exciting journey to discover the true value of your car and make informed decisions in the automotive market!
 """)
