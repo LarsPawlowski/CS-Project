98d90f4909ebd96df5fa74fe3036dabb96921fc0
Lars Pawlowski <larspaw@gmx.ch>
2026-04-21 14:03:16 +0200

    Website Description

--- CS_Group_Project.py
+++ CS_Group_Project.py
@@ -10,8 +10,7 @@ st.set_page_config(page_title="AutoAppraise - Your Cars True Value", page_icon="
 # Title & Intro
 st.title("Welcome to AutoAppraise!")
 st.write("""
-Welcome to the interactive Streamlit tutorial tailored for FCS bachelor students! 
-Here's a introductory showcase of what you can achieve using Streamlit with very little code.
+Welcome to AutoAppraise, the ultimate tool for car enthusiasts and buyers alike! Our mission is to provide you with accurate and up-to-date information about your car's true value. Whether you're looking to sell, buy, or simply curious about your vehicle's worth, AutoAppraise has got you covered. With our user-friendly interface and powerful algorithms, you can easily get an estimate of your car's value based on its make, model, year, mileage, and condition. Join us on this exciting journey to discover the true value of your car and make informed decisions in the automotive market!
 """)
 
 # Text Input
